bc765a33f869f59f88016accdff8c12acb531962
akira345 <g.akira345@gmail.com>
2016-08-27 22:39:33 +0900

    容量の取得法を変更。

--- zerosim_check.rb
+++ zerosim_check.rb
@@ -34,7 +34,8 @@ SQLite3::Database.new("zero.db") do |db|
 end
 
 # 会員ページへログイン
-data = []
+today_data = 0
+before_yesterday_data = 0
 agent = Mechanize.new
 agent.user_agent_alias = 'Windows IE 7'
 agent.get(url) do | page |
@@ -46,21 +47,23 @@ agent.get(url) do | page |
   # 容量を取得
   res = mypage.form_with(name:'userUsageActionForm').submit
   html = Nokogiri::HTML.parse(res.body)
-  html.css('div').css('.guideSignElem').css('dd').each do | d |
-    data.push(d.children.text.gsub(/(\s)/,"").gsub(/MB$/,"").to_i)
-  end
+  today_data = html.xpath('//dl[@class="useConditionDisplay"]/dt[contains(text(),"今月のデータ使用量(速報値)")]/following-sibling::dd').text.gsub(/(\s)/,"").gsub(/MB$/,"").to_i
+  before_yesterday_data = html.xpath('//dl[@class="useConditionDisplay"]/dt[contains(text(),"一昨日のデータ使用量")]/following-sibling::dd').text.gsub(/(\s)/,"").gsub(/MB$/,"").to_i
+
 end
+pp before_yesterday_data
+pp today_data
 # データベースに格納
 SQLite3::Database.new("zero.db") do |db|
   db.prepare('REPLACE INTO use_data (use_year,use_month,use_data) VALUES (?,?,?)') do |stmt|
     # 今月の容量
     today = Date.today
-    stmt.execute(today.year,today.month,data[0])
+    stmt.execute(today.year,today.month,today_data)
     # 会員サイトでは、先月の容量が確認できないので、
     # 一昨日が先月だったら、先月の容量が確定しているものとして更新する。
     day_before_yesterday = today -3.day
     if today.month != day_before_yesterday.month
-      stmt.execute(day_before_yesterday.year,day_before_yesterday.month,data[3])
+      stmt.execute(day_before_yesterday.year,day_before_yesterday.month,before_yesterday_data)
     end
   end
   # ZeroSimは３か月未利用だと自動解約されるので、直近２か月未利用だったら警告を出す。
@@ -82,7 +85,7 @@ SQLite3::Database.new("zero.db") do |db|
   end
 end
 # 容量上限チェック
-if data[0] >= 400
+if today_data >= 400
   mail = Mail.new do
     from mail_from
     to mail
